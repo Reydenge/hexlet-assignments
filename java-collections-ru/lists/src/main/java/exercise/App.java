5918bb35c00a4a2bfcd66c6787aa1ccc197b5acf
@hexlet/cli <support@hexlet.io>
2022-11-01 19:55:01 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -7,25 +7,17 @@ import java.util.List;
 // BEGIN
 class App {
     public static boolean scrabble (String characters, String word) {
-        List<String> charList = new ArrayList<>();
-        List<String> wordList = new ArrayList<>();
-        String char1 = characters.toLowerCase();
-        String word1 = word.toLowerCase();
-        int count = 0;
-        for (int i = 0; i < char1.length(); i++) {
-            charList.add(String.valueOf(char1.charAt(i)));
-        }
-        for (int j = 0; j < word1.length(); j++) {
-            wordList.add(String.valueOf(word1.charAt(j)));
-        }
-        int sizeOfCharList = charList.size();
-        for (String s : wordList) {
-            if (charList.contains(s)) {
-                charList.remove(s);
+        String[] char1 = characters.toLowerCase().split("");
+        ArrayList charList = new ArrayList(Arrays.asList(char1));
+        String[] word1 = word.toLowerCase().split("");
+        ArrayList wordList = new ArrayList(Arrays.asList(word1));
+        for (Object s : wordList) {
+            if (!charList.contains(s)) {
+                return false;
             }
+            charList.remove(s);
         }
-        int diff = sizeOfCharList - charList.size();
-        return word.length()==diff;
+        return true;
         }
     }
 
