0002e48955e99c69d0dffd71b738f929659fc46a
@hexlet/cli <support@hexlet.io>
2022-11-03 21:42:58 +0300

    submit java-collections-ru/maps

--- java-collections-ru/maps/src/main/java/exercise/App.java
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -7,6 +7,9 @@ class App {
     public static Map<String, Integer> getWordCount(String sentence) {
         Map<String, Integer> wordsMap = new HashMap<String, Integer>();
         String[] wordsFromSentence = sentence.split(" ");
+        if (sentence.length() == 0) {
+            return wordsMap;
+        }
         for (String pair : wordsFromSentence) {
             int count = 0;
             for (String s : wordsFromSentence) {
@@ -20,14 +23,23 @@ class App {
     }
 
     public static String toString(Map<String, Integer> wordsMap) {
-        String[] s0 = new String[wordsMap.size()];
-        for (Map.Entry<String, Integer> pair : wordsMap.entrySet()) {
-            for (int i = 0; i < s0.length; i++) {
-                StringBuilder s1 = new StringBuilder(pair.getKey() + ": ");
-                s0[i] = "  " + s1.append(pair.getValue());
-            }
+        if (wordsMap.isEmpty()) {
+            return "{}";
         }
-        return Arrays.toString(s0);
+        StringBuilder result = new StringBuilder("{\n");
+        for (Map.Entry<String, Integer> pair : wordsMap.entrySet()) {
+            result = result.append("  " + pair.getKey() + ": " + pair.getValue() + "\n");
+            }
+        String result2 = String.valueOf(result.append("}"));
+        return result2;
+        }
+
+
+        public static void main (String[] args) {
+            String sentence1 = "word text cat apple word map apple word";
+            Map s1 = App.getWordCount(sentence1);
+            String s2 = App.toString(s1);
+            System.out.println(s2);
+        }
     }
-}
 //END
