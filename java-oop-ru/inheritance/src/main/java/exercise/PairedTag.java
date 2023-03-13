a20384f50d9f23b66f54d9f8eaaee1d4c82f4474
@hexlet/cli <support@hexlet.io>
2023-03-13 13:27:43 +0300

    submit java-oop-ru/inheritance

--- java-oop-ru/inheritance/src/main/java/exercise/PairedTag.java
+++ java-oop-ru/inheritance/src/main/java/exercise/PairedTag.java
@@ -5,18 +5,18 @@ import java.util.List;
 // BEGIN
 public class PairedTag extends Tag {
     private String tagBody;
-    private List<Tag> children;
+    private List<Tag> listOfTags;
 
     public PairedTag (String tag, Map<String, String> attributes, String tagBody, List<Tag> children) {
         super(tag, attributes);
         this.tagBody = tagBody;
-        this.children = children;
+        this.listOfTags = children;
     }
 
     @Override
     public String toString() {
         StringBuilder result = new StringBuilder(super.toString());
-        for (var child : children) {
+        for (var child : listOfTags) {
             result.append(child.toString());
         }
         result.append(tagBody).append("</").append(tag).append(">");
