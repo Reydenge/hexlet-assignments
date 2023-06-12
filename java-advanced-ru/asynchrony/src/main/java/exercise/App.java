30d0f22a7a827feba620deade3c173b891db8835
@hexlet/cli <support@hexlet.io>
2023-06-12 18:39:22 +0300

    submit java-advanced-ru/asynchrony

---
+++ java-advanced-ru/asynchrony/src/main/java/exercise/App.java
@@ -0,0 +1,53 @@
+package exercise;
+
+import java.io.IOException;
+import java.nio.file.StandardOpenOption;
+import java.util.concurrent.CompletableFuture;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+
+import static java.nio.file.Files.write;
+import static java.nio.file.Files.readString;
+
+class App {
+
+    // BEGIN
+    public static CompletableFuture<String> unionFiles(String firstFile, String secondFile, String unionFile) {
+        CompletableFuture<String> content1 = getText(firstFile);
+        CompletableFuture<String> content2 = getText(secondFile);
+
+        return content1.thenCombine(content2, (fileFuture1, fileFuture2) -> {
+            Path path = Paths.get(unionFile);
+
+            try {
+                write(path, (fileFuture1 + fileFuture2).getBytes(), StandardOpenOption.CREATE);
+            } catch (IOException error) {
+                System.out.println("No such file exists");
+            }
+            return "Success";
+        });
+    }
+
+    private static CompletableFuture<String> getText(String file) {
+
+        return CompletableFuture.supplyAsync(() -> {
+            Path path = Paths.get(file);
+            String content = "";
+
+            try {
+                content = readString(path);
+            } catch (IOException error) {
+                System.out.println("No such file exists");
+            }
+            return content;
+        });
+    }
+    // END
+    public static void main(String[] args) throws Exception {
+        // BEGIN
+        System.out.println(unionFiles("src/main/resources/file1.txt",
+                        "src/main/resources/file2.txt",
+                        "result.txt").get());
+        // END
+    }
+}
